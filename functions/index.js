80ae3ae19b9c088f15917895399f4e1a6e8f5999
Kevin Qi <kevin.t.qi@gmail.com>
2018-05-07 18:54:31 -0700

    Correct interface

--- functions/index.js
+++ functions/index.js
@@ -10,24 +10,12 @@ const NotificationPublisher = require('./src/notification-publisher');
 admin.initializeApp();
 
 const publisher = new NotificationPublisher(admin);
-// POST /api/v1/action {"title": "title", "body": "body"}
-exports.publishNotification = functions.https.onRequest((req, res) => {
-    console.log(req.body);
-    publisher.send(req.body)
-        .then(() => {
-            return res.status(200).send("ok");
-        })
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).send(err.message);
-        });
-});
 
 exports.intentFulfillment = functions.https.onRequest((req, res) => {
     console.log(req.body.queryResult);
     const data = {
         title: req.body.queryResult.action,
-        body: req.body.queryResult.parameters.squad
+        body: JSON.stringify(req.body.queryResult.parameters)
     }
     publisher.send(data).then(() => {
             return res.status(200).end();
