a69e3b9d91166716be7234c03d1aa15e7d07bb3a
Kevin Qi <kevin.t.qi@gmail.com>
2018-05-19 14:42:53 -0700

    No error if no subscriber

--- functions/src/subscriber.js
+++ functions/src/subscriber.js
@@ -1,14 +1,19 @@
 class Subscriber {
     constructor(admin) {
         this.admin = admin;
+        this.tokens = [];
     }
 
     getTokens() {
-        return this.admin.database().ref(`notificationTokens`).once('value')
+        return this.admin.database().ref('notificationTokens/userid').once('value')
             .then((snapshot) => {
-                return Object.keys(snapshot.val());
+                if (snapshot.val()) {
+                    this.tokens = Object.values(snapshot.val());
+                } 
+                console.log(JSON.stringify(this.tokens));
+                return this.tokens;
             });
     }
 }
 
-module.exports = Subscriber
+module.exports = Subscriber;
